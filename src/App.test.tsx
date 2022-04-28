d9e45a9f96eae7714ecf49d8b08082e8aa3a5aab
Tomasz <skretkowskitomasz@gmail.com>
2022-04-29 00:36:30 +0200

    Packages and config

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import React from "react";
+import { Provider } from "react-redux";
+import { render } from "@testing-library/react";
 
-test('renders learn react link', () => {
+import App from "./App";
+import { store } from "./app/store";
+
+test("renders learn react link", () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
     </Provider>
   );
 
+  // eslint-disable-next-line testing-library/prefer-screen-queries
   expect(getByText(/learn/i)).toBeInTheDocument();
 });
