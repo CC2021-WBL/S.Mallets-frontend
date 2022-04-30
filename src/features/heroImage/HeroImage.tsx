c90fd7c5934e391db00fda4dc86dcfd925452fa8
Robert <elgreen@tlen.pl>
2022-04-30 20:20:46 +0200

    fix: fixed errors

---
+++ src/features/heroImage/HeroImage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function HeroImage() {
+  return <div>HeroImage</div>;
+}
+
+export default HeroImage;
---
+++ src/features/layout/cart/Cart-spec.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CartSpec() {
+  return <div>Cart-spec</div>;
+}
+
+export default CartSpec;
