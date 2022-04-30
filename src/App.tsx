af21414d107f074c2e3781d17d40871c498a148e
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-04-30 22:50:39 +0200

    SM-13 test3

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { I18nextProvider } from 'react-i18next';
+import { I18nextProvider, useTranslation } from 'react-i18next';
 import { Suspense } from 'react';
-
-import i18n from './i18n';
 
 const Loader = () => (
   <div className="App">
@@ -9,11 +7,28 @@ const Loader = () => (
   </div>
 );
 function App() {
+  const { t, i18n } = useTranslation('navFooter');
+
+  const changeLanguage = (lng: any) => {
+    i18n.changeLanguage(lng);
+  };
   return (
     <I18nextProvider i18n={i18n}>
       <Suspense fallback={<Loader />}>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h1 className="text-3xl font-bold underline">
+          Hello world! {t('account')}
+        </h1>
         <a href="https://github.com/orgs/CC2021-WBL/">FB</a>
+        <div>
+          <button type="button" onClick={() => changeLanguage('pl')}>
+            pl
+          </button>
+          <button type="button" onClick={() => changeLanguage('en')}>
+            en
+          </button>
+        </div>
+        <p>{t('account')}</p>
+        <p> {t('footer.about')}</p>
       </Suspense>
     </I18nextProvider>
   );
