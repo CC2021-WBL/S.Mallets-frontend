4740a09ab25a933cf52706aa0b8e253d1c0924e4
Tomasz Skrętkowski <skretkowskitomasz@gmail.com>
2022-05-01 13:05:05 +0200

    SM-45 Removed redux package; Added new eslint rule (#4)

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
+import './index.css';
+
 import React from 'react';
-import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { createRoot } from 'react-dom/client';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
+import { store } from './app/store';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -14,7 +16,7 @@ root.render(
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
