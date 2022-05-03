56463089a0aad8e7c8fd3b45a6250c321c8c0c82
Tomasz <skretkowskitomasz@gmail.com>
2022-05-03 20:13:47 +0200

    SM-35 Added: Mock img and container styles

--- src/features/products/ProductsPage.tsx
+++ src/features/products/ProductsPage.tsx
@@ -1,10 +1,35 @@
+const mockImg =
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==';
+
 const ProductsPage = () => {
+  // const [imgUrl, setImgUrl] = useState<any | null>(null);
+
+  // useEffect(() => {
+  //   const getImg = async () => {
+  //     try {
+  //       const response = await fetch(prop.serviceObject.image);
+  //       if (!response.ok) {
+  //         throw new Error();
+  //       } else {
+  //         const imageBlob = await response.blob();
+  //         const reader = new FileReader();
+  //         reader.readAsDataURL(imageBlob);
+  //         reader.onloadend = () => {
+  //           const base64data = reader.result;
+  //           setImgUrl(base64data);
+  //         };
+  //       }
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+  //   getImg();
+  // }, []);
+
   return (
-    <>
-      <div className="container mx-auto  flex max-w-7xl bg-[#4d514d] p-4">
-        ProductsPage
-      </div>
-    </>
+    <div className="container mx-auto grid max-w-[1240px]">
+      <img src={mockImg} alt="Red dot" width="100px" />
+    </div>
   );
 };
 
