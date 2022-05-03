d0c4cf5ec3978ba0d31830e914e647c0b5267895
Tomasz <skretkowskitomasz@gmail.com>
2022-05-03 17:54:56 +0200

    SM-35 Added Tailwind prettier plugin

--- src/features/products/ProductsPage.tsx
+++ src/features/products/ProductsPage.tsx
@@ -1,7 +1,7 @@
 const ProductsPage = () => {
   return (
     <>
-      <div className="container flex max-w-7xl mx-auto p-4 bg-[#4d514d]">
+      <div className="container mx-auto  flex max-w-7xl bg-[#4d514d] p-4">
         ProductsPage
       </div>
     </>
