b000591f5a1d6ad030df157bd7ae7b9417d7c914
Tomasz <skretkowskitomasz@gmail.com>
2022-05-04 21:57:42 +0200

    SM-42 Lint Staged rule Changed

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -2,6 +2,6 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 import type { AppDispatch, RootState } from './store';
 
-// Use throughout your app instead of plain `useDispatch` and `useSel
+// Use throughout your app instead of plain `useDispatch` and `useSelection
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
