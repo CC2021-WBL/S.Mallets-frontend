469165e3a60bfc5d3378b71050f17ea3b2ec1b5f
Tomasz <skretkowskitomasz@gmail.com>
2022-05-04 21:47:12 +0200

    Husky test

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -2,6 +2,6 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 import type { AppDispatch, RootState } from './store';
 
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
+// Use throughout your app instead of plain `useDispatch` and `useSel
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
