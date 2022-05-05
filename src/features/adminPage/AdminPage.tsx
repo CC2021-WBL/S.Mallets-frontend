478a9d40bec7fce010f71e1403e8c8acc76254bd
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-05-05 19:50:46 +0200

    SM-13 removed unnecessary styling

--- src/features/adminPage/AdminPage.tsx
+++ src/features/adminPage/AdminPage.tsx
@@ -1,18 +1,4 @@
-const mainStyle = {
-  background: 'grey',
-  display: 'flex',
-  margin: '0 auto',
-  width: '80%',
-  height: 'calc(100vh - 4rem)',
-  justifyContent: 'space-between',
-  padding: '1rem 1rem',
-};
-
 const AdminPage = () => {
-  return (
-    <div className="text-3xl font-bold underline" style={mainStyle}>
-      AdminPage
-    </div>
-  );
+  return <div>AdminPage</div>;
 };
 export default AdminPage;
--- src/features/homePage/HomePage.tsx
+++ src/features/homePage/HomePage.tsx
@@ -1,19 +1,5 @@
-const mainStyle = {
-  background: 'grey',
-  display: 'flex',
-  margin: '0 auto',
-  width: '80%',
-  height: 'calc(100vh - 4rem)',
-  justifyContent: 'space-between',
-  padding: '1rem 1rem',
-};
-
 const HomePage = () => {
-  return (
-    <div className="text-3xl font-bold underline" style={mainStyle}>
-      HomePage
-    </div>
-  );
+  return <div>HomePage</div>;
 };
 
 export default HomePage;
