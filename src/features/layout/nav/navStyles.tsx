79131010259fb8c941fe0b7aa50c7a282fe63546
Tomasz <skretkowskitomasz@gmail.com>
2022-05-07 11:27:52 +0200

    SM-33 Active styles added

--- src/features/layout/nav/navStyles.tsx
+++ src/features/layout/nav/navStyles.tsx
@@ -1,3 +1,3 @@
 export const navBurgerStyles = 'p-4 border-b border-grey-600 hover:underline';
 export const navStyles =
-  'text-sm p-3 md:p-4 hover:underline focus:font-medium focus:underline';
+  'text-sm p-3 md:p-4 hover:underline focus:underline active:font-medium';
