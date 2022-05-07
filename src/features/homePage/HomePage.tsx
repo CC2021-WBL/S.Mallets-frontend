43f44ced77121335f84cac1117175e2e8ff294f8
Robert <elgreen@tlen.pl>
2022-05-08 00:53:02 +0200

    feat: featuring svg logo

--- src/features/homePage/HomePage.tsx
+++ src/features/homePage/HomePage.tsx
@@ -1,5 +1,19 @@
+// import loader from '../../assets/smalletsLogo.svg';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <div style={{ position: 'absolute', left: '50%' }}>
+        HomePage
+        <img
+          src={require('../../assets/smalletsLogo.svg')}
+          alt="logo"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
