0f2f9a3deba6587d922d0c7d1652d42e05137640
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-05-08 15:07:05 +0200

    SM-58 added more styling to hero

--- src/features/homePage/HomePage.tsx
+++ src/features/homePage/HomePage.tsx
@@ -1,3 +1,5 @@
+import group from '../../assets/groupLogo.png';
+
 const HomePage = () => {
   return (
     <>
@@ -7,7 +9,21 @@ const HomePage = () => {
           Premium quality / fair price
         </h1>
 
-        <div className="max-w-[940px] bg-[#EDEDED] mx-auto h-60 absolute -bottom-40 inset-x-0"></div>
+        <div className="max-w-[58.7rem] bg-[#EDEDED] mx-auto h-60 absolute -bottom-40 inset-x-0 text-black flex justify-center text-left pt-10 sm:justify-start sm:pl-[4.4rem] rounded-lg">
+          <blockquote>
+            <p className=" max-w-[17rem] mb-5 ">
+              “To są zajebiste pałeczki. Nie chcę już żadnych innych. Mój
+              dyrygent jest wreszcie zadowolony, tylko Smallets!”
+            </p>
+            <p className="font-bold">Klemens Wariat</p>
+          </blockquote>
+        </div>
+        <img
+          src={group}
+          alt="decoration logo"
+          className="z-50 hidden xl:block absolute right-[2.8rem] -bottom-[15.6rem]"
+          aria-hidden="true"
+        />
       </div>
     </>
   );
