60330a529e02ee0eee21de57183de57b680c5882
Robert <elgreen@tlen.pl>
2022-05-08 11:37:18 +0200

    SM-31 feat: animated loader draft

--- src/features/homePage/HomePage.tsx
+++ src/features/homePage/HomePage.tsx
@@ -1,16 +1,37 @@
-// import loader from '../../assets/smalletsLogo.svg';
-
 const HomePage = () => {
   return (
     <>
-      <div style={{ position: 'absolute', left: '50%' }}>
-        HomePage
-        <img
-          src={require('../../assets/smalletsLogo.svg')}
-          alt="logo"
-          width="100%"
-          height="100%"
-        />
+      HomePage
+      <div
+        style={{
+          position: 'absolute',
+          left: '50%',
+          width: '100px', // this decides on svg with (and height respectively
+          // outline: '.5px dotted grey',
+          margin: '0 auto',
+        }}
+      >
+        <svg
+          version="1.1"
+          viewBox="0 0 110 110"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g
+            transform="translate(-56.666 -83.368)"
+            fill="none"
+            strokeLinecap="round"
+            strokeWidth="10"
+          >
+            <path
+              className="animate-path1dance origin-right"
+              d="m157.54 148.49-45.873-24.098m-45.893 3.9379a46.977 46.977 0 0 1 53.303-36.353 46.977 46.977 0 0 1 38.978 53.8 46.977 46.977 0 0 1-0.51569 2.7134"
+            />
+            <path
+              className="animate-path2dance origin-right"
+              d="m65.777 128.33 45.892 24.108m45.873-3.9481a46.977 46.977 0 0 1-53.284 36.265 46.977 46.977 0 0 1-38.978-53.8 46.977 46.977 0 0 1 0.49628-2.625"
+            />
+          </g>
+        </svg>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,46 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        path1dance: 'path1dance 1.5s linear alternate 0s infinite',
+        path2dance: 'path2dance 1.5s linear alternate 0s infinite',
+      },
+      keyframes: {
+        path1dance: {
+          '0%': {
+            strokeDashoffset: '201.4988250732422px',
+            strokeDasharray: '201.4988250732422px',
+            transform: 'rotateY(0deg)',
+            stroke: '#000',
+          },
+          '70%, 100%': {
+            strokeDashoffset: '402.9976501464844px',
+            strokeDasharray: '201.4988250732422px',
+          },
+          '100%': {
+            transform: 'rotateY(360deg)',
+            stroke: '#ccc',
+          },
+        },
+        path2dance: {
+          '0%': {
+            strokeDashoffset: '201.3398895263672px',
+            strokeDasharray: '201.3398895263672px',
+            transform: 'rotateY(0deg)',
+            stroke: '#000',
+          },
+          '20%, 100%': {
+            strokeDashoffset: '402.9976501464844px',
+            strokeDasharray: '201.4988250732422px',
+          },
+          '100%': {
+            transform: 'rotateY(360deg)',
+            stroke: '#ccc',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
