104e177558972f5d310bfd286bd07ed7ff36dec3
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-05-08 12:06:44 +0200

    SM-58 added hero image, little styling

--- src/features/homePage/HomePage.tsx
+++ src/features/homePage/HomePage.tsx
@@ -1,5 +1,16 @@
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <div className="mx-auto max-w-7xl bg-no-repeat  hero flex flex-col items-end text-right text-white pr-8 relative">
+        <p className="mb-2.5 mt-16">S.MALLETS</p>
+        <h1 className="max-w-sm text-6xl font-bold leading-[4.8rem]">
+          Premium quality / fair price
+        </h1>
+
+        <div className="max-w-[940px] bg-[#EDEDED] mx-auto h-60 absolute -bottom-40 inset-x-0"></div>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
