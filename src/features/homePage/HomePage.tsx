9cc772f4c9ed3ebbe0ff0766f0b781a3b58745f2
Robert <elgreen@tlen.pl>
2022-05-09 23:26:32 +0200

    SM-31 feat: added alternative animation

--- src/features/homePage/HomePage.tsx
+++ src/features/homePage/HomePage.tsx
@@ -7,7 +7,7 @@ const HomePage = () => {
           position: 'absolute',
           left: '50%',
           width: '100px', // this decides on svg with (and height respectively
-          // outline: '.5px dotted grey',
+          outline: '.5px dotted grey',
           margin: '0 auto',
         }}
       >
@@ -28,6 +28,27 @@ const HomePage = () => {
             />
             <path
               className="animate-path2dance origin-right"
+              d="m65.777 128.33 45.892 24.108m45.873-3.9481a46.977 46.977 0 0 1-53.284 36.265 46.977 46.977 0 0 1-38.978-53.8 46.977 46.977 0 0 1 0.49628-2.625"
+            />
+          </g>
+        </svg>
+        <svg
+          version="1.1"
+          viewBox="0 0 110 110"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g
+            transform="translate(-56.666 -83.368)"
+            fill="none"
+            strokeLinecap="round"
+            strokeWidth="10"
+          >
+            <path
+              className="animate-path3dance origin-right"
+              d="m157.54 148.49-45.873-24.098m-45.893 3.9379a46.977 46.977 0 0 1 53.303-36.353 46.977 46.977 0 0 1 38.978 53.8 46.977 46.977 0 0 1-0.51569 2.7134"
+            />
+            <path
+              className="animate-path4dance origin-right"
               d="m65.777 128.33 45.892 24.108m45.873-3.9481a46.977 46.977 0 0 1-53.284 36.265 46.977 46.977 0 0 1-38.978-53.8 46.977 46.977 0 0 1 0.49628-2.625"
             />
           </g>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
       animation: {
         path1dance: 'path1dance 1.5s linear alternate 0s infinite',
         path2dance: 'path2dance 1.5s linear alternate 0s infinite',
+        path3dance: 'path3dance 1.5s linear alternate 0s infinite',
+        path4dance: 'path4dance 1.5s linear alternate 0s infinite',
       },
       keyframes: {
         path1dance: {
@@ -36,6 +38,38 @@ module.exports = {
           },
           '100%': {
             transform: 'rotateY(360deg)',
+            stroke: '#ccc',
+          },
+        },
+        path3dance: {
+          '0%': {
+            strokeDashoffset: '201.4988250732422px',
+            strokeDasharray: '201.4988250732422px',
+            // transform: 'rotateY(0deg)',
+            stroke: '#000',
+          },
+          '70%, 100%': {
+            strokeDashoffset: '402.9976501464844px',
+            strokeDasharray: '201.4988250732422px',
+          },
+          '100%': {
+            // transform: 'rotateY(360deg)',
+            stroke: '#ccc',
+          },
+        },
+        path4dance: {
+          '0%': {
+            strokeDashoffset: '201.3398895263672px',
+            strokeDasharray: '201.3398895263672px',
+            // transform: 'rotateY(0deg)',
+            stroke: '#000',
+          },
+          '20%, 100%': {
+            strokeDashoffset: '402.9976501464844px',
+            strokeDasharray: '201.4988250732422px',
+          },
+          '100%': {
+            // transform: 'rotateY(360deg)',
             stroke: '#ccc',
           },
         },
