930ea7d542c150e10b2274b8f019e5b1d7be1e22
Tomasz <skretkowskitomasz@gmail.com>
2022-05-09 17:21:00 +0200

    SM-35 Added Modal Mock

--- src/features/products/ProductCard.tsx
+++ src/features/products/ProductCard.tsx
@@ -1,3 +1,7 @@
+import { useState } from 'react';
+
+import Backdrop from '../layout/nav/Backdrop';
+
 interface productDataType {
   id: number;
   productName: string;
@@ -12,49 +16,65 @@ interface productDataType {
 }
 
 const ProductCard = (prop: { productObject: productDataType }) => {
+  const [detailsModal, setDetailsModal] = useState(false);
+  const handleDetailsModal = () => {
+    setDetailsModal(!detailsModal);
+  };
   return (
-    <div className="relative z-10 w-screen">
-      <div
-        className="flex w-full max-w-[810px] grow
+    <>
+      <div className="relative z-10 w-screen">
+        <div
+          className="flex w-full max-w-[810px] grow
      flex-col justify-between p-6 sm:h-fit sm:w-full sm:p-6 md:flex-row md:p-10"
-      >
-        <div className="md:w-64">
-          <img
-            src={prop.productObject.productImage}
-            alt="Red dot"
-            className="mb-3 h-36 w-full shadow-none transition-shadow duration-300 ease-in-out hover:shadow-xl 
-          md:mb-0 md:h-full md:w-full"
-          />
-        </div>
-        <div className="card-data flex w-full flex-col justify-between gap-5 md:pl-4">
-          <div className="flex h-10 items-center justify-between text-xl font-bold leading-6">
-            <h3 className="model">{prop.productObject.productName}</h3>
-            <div className="price h-full rounded-md border border-black p-2 text-center">
-              {prop.productObject.price} € / para
-            </div>
+        >
+          <div className="md:w-64">
+            <img
+              src={prop.productObject.productImage}
+              alt="Red dot"
+              className="mb-3 h-36 w-full cursor-pointer shadow-none transition-shadow duration-300 ease-in-out 
+          hover:shadow-xl md:mb-0 md:h-full md:w-full"
+              onClick={handleDetailsModal}
+              aria-hidden="true"
+            />
           </div>
-          <p className="text-lg sm:pr-20 sm:text-xl">
-            {prop.productObject.productDescription}
-          </p>
-          <div className="flex h-fit flex-col sm:h-12 sm:flex-row sm:justify-between">
-            <div>
-              <p className="py-1 text-sm sm:py-0 sm:text-base">
-                Head Diameter: {prop.productObject.headDiameter} mm Ø
-              </p>
-              <p className="text-sm sm:py-0 sm:text-base">
-                Stick length: {prop.productObject.stickLength} cm
-              </p>
+          <div className="card-data flex w-full flex-col justify-between gap-5 md:pl-4">
+            <div className="flex h-10 items-center justify-between text-xl font-bold leading-6">
+              <h3 className="model">{prop.productObject.productName}</h3>
+              <div className="price h-full rounded-md border border-black p-2 text-center">
+                {prop.productObject.price} € / para
+              </div>
             </div>
-            <button
-              className="mt-10 h-12 w-56 rounded-md bg-[#232323] px-6 font-bold uppercase text-white shadow-none transition-shadow duration-300 
+            <p className="text-lg sm:pr-20 sm:text-xl">
+              {prop.productObject.productDescription}
+            </p>
+            <div className="flex h-fit flex-col sm:h-12 sm:flex-row sm:justify-between">
+              <div>
+                <p className="py-1 text-sm sm:py-0 sm:text-base">
+                  Head Diameter: {prop.productObject.headDiameter} mm Ø
+                </p>
+                <p className="text-sm sm:py-0 sm:text-base">
+                  Stick length: {prop.productObject.stickLength} cm
+                </p>
+              </div>
+              <button
+                className="mt-10 h-12 w-56 rounded-md bg-[#232323] px-6 font-bold uppercase text-white shadow-none transition-shadow duration-300 
           ease-in-out hover:shadow-xl sm:mt-0"
-            >
-              Dodaj do koszyka
-            </button>
+              >
+                Dodaj do koszyka
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      {detailsModal && (
+        <>
+          <h1 className="absolute top-1/2 left-1/2 z-30 -translate-x-1/2 -translate-y-1/2 bg-white text-6xl">
+            PRODUCT DETAILS
+          </h1>
+          <Backdrop handleClose={handleDetailsModal} />
+        </>
+      )}
+    </>
   );
 };
 
