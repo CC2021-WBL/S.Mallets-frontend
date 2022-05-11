2785c6664049f45c6306c2cd71162db04a3d9a06
Robert <elgreen@tlen.pl>
2022-05-11 22:44:02 +0200

    SM-31 fix: removed loader from HomePage

--- src/features/homePage/HomePage.tsx
+++ src/features/homePage/HomePage.tsx
@@ -1,10 +1,7 @@
-import { Loader } from '../Loader';
-
 const HomePage = () => {
   return (
     <>
       <h1 className="p-8 text-center text-6xl">Landing Page</h1>
-      <Loader />
     </>
   );
 };
