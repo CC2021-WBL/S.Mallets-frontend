f700a788a3445f8749c1d8d261c093d23bd831a5
Tomasz <skretkowskitomasz@gmail.com>
2022-05-13 00:55:11 +0200

    SM-58 Final amendments

--- src/features/homePage/HomePage.tsx
+++ src/features/homePage/HomePage.tsx
@@ -1,15 +1,16 @@
 /* eslint-disable import/named */
 
 import * as Yup from 'yup';
+import emailjs from '@emailjs/browser';
 import React, { useState } from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
 import { ErrorMessage, Field, Form, Formik, FormikHelpers } from 'formik';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 import group from '../../assets/groupLogo.png';
+import logosm from '../../assets/logosmall.png';
 import { mockDataType } from '../../assets/mockData/mockSeriesData';
-
-// import emailjs from '@emailjs/browser';
 
 interface IFormValues {
   email: string;
@@ -30,19 +31,17 @@ const HomePage: React.FC<{ mockSeries: mockDataType[] }> = (props) => {
 
   return (
     <div className="mx-auto w-full max-w-7xl">
-      <div className="hero relative mx-auto  mb-[15.6rem] flex max-w-7xl flex-col items-end bg-no-repeat pr-8 text-right text-slate-500 sm:text-white">
-        <p className="mb-2.5 mt-16">S.MALLETS</p>
-        <h1 className="max-w-sm text-5xl font-bold leading-[4.8rem]">
+      <div className="hero relative mx-auto mb-7 flex max-w-7xl flex-col items-end bg-no-repeat pr-8 text-right sm:mb-56 sm:text-white">
+        <p className="mb-2.5 mt-16 hidden sm:block">S.MALLETS</p>
+        <h1 className="hidden max-w-sm text-5xl font-bold leading-[4.8rem] sm:block">
           Premium quality / fair price
         </h1>
-
-        <div className="absolute inset-x-0 -bottom-40 mx-auto flex h-60 max-w-[80%] justify-center rounded-lg bg-[#EDEDED] p-10 text-left text-black sm:justify-start sm:pl-[4.4rem] lg:max-w-[58.7rem]">
+        <div className="absolute inset-x-0 bottom-20 mx-auto flex h-60 max-w-[80%] justify-center rounded-lg bg-[#E5E1C6] bg-opacity-60 from-[#E5E1C6] to-transparent p-10 text-left text-black sm:-bottom-40 sm:justify-start sm:bg-[#EDEDED] sm:bg-gradient-to-b sm:pl-[4.4rem] lg:max-w-[58.7rem]">
           <blockquote>
             <p className="mb-5 max-w-[17rem]">{t('opinion')}</p>
             <p className="font-bold">Klemens Wariat</p>
           </blockquote>
         </div>
-
         <img
           src={group}
           alt="decoration logo"
@@ -52,18 +51,16 @@ const HomePage: React.FC<{ mockSeries: mockDataType[] }> = (props) => {
       </div>
 
       <div>
-        <h2 className="ml-6 mb-9 text-center text-2xl sm:mb-0 sm:text-left">
-          {t('subtitle')}
-        </h2>
+        <h2 className="mb-9 text-center text-2xl sm:mb-6">{t('subtitle')}</h2>
       </div>
-      <div className=" relative mb-[6.3rem] flex max-w-7xl flex-wrap justify-center">
+      <div className="relative flex max-w-7xl flex-wrap justify-center sm:mb-16">
         {props.mockSeries
           .sort(() => 0.5 - Math.random())
           .slice(0, 6)
           .map((index: mockDataType) => (
             <Link
               key={index.id}
-              className="mb-10 flex h-[18.7rem] w-[21.8rem] flex-col items-center py-0 hover:cursor-pointer sm:py-9 sm:hover:shadow-lg md:h-[27.8rem]"
+              className="mb-14 flex w-[21.8rem] flex-col items-center py-0 hover:cursor-pointer sm:mb-6 sm:rounded-lg sm:py-9 sm:hover:shadow-lg md:h-[27.8rem]"
               to="/product-series"
             >
               <img
@@ -81,7 +78,7 @@ const HomePage: React.FC<{ mockSeries: mockDataType[] }> = (props) => {
           ))}
       </div>
 
-      <div className=" mx-6 mb-[3.1rem] flex max-w-7xl justify-between sm:px-3 md:px-6 lg:px-8">
+      <div className="mx-6 mb-20 flex max-w-7xl justify-between sm:px-3 md:px-6 lg:px-8">
         <Formik
           initialValues={{
             email: '',
@@ -95,23 +92,23 @@ const HomePage: React.FC<{ mockSeries: mockDataType[] }> = (props) => {
             actions.setSubmitting(true);
             actions.resetForm();
             setShowModal(true);
-            // emailjs
-            //   .send(
-            //     'service_ypr0zj8',
-            //     'template_z0d7tqj',
-            //     {
-            //       email: values.email,
-            //       message: values.message,
-            //     },
-            //     'user_pR6XzZUshqc9XuxuBLUzf',
-            //   )
-            //   .then(() => {
-            //     actions.setSubmitting(false);
-            //     actions.resetForm();
-            //   });
+            emailjs
+              .send(
+                'service_ypr0zj8',
+                'template_z0d7tqj',
+                {
+                  email: values.email,
+                  message: values.message,
+                },
+                'user_pR6XzZUshqc9XuxuBLUzf',
+              )
+              .then(() => {
+                actions.setSubmitting(false);
+                actions.resetForm();
+              });
           }}
         >
-          <div className="h-fit-content  w-full lg:w-[50%] ">
+          <div className="h-fit-content mx-auto w-11/12 md:w-8/12 lg:w-7/12 lg2:w-1/2 ">
             <h2 className="pb-7 text-2xl">{t('more')}</h2>
             <h3 className="pb-7">{t('contactForm')}</h3>
             {showModal ? (
@@ -124,20 +121,21 @@ const HomePage: React.FC<{ mockSeries: mockDataType[] }> = (props) => {
                 <div className="fixed top-0 left-0  z-20 h-full w-full bg-black opacity-20" />
                 {/* czcionka różna */}
                 <div
-                  className="  fixed top-[50%] left-[50%]  z-30 flex h-[12.5rem] w-[12.5rem] -translate-y-1/2 -translate-x-1/2 items-center justify-center rounded-lg bg-white p-4 shadow-xl  hover:cursor-default sm:h-[50%] sm:w-[50%] "
+                  className="bg- fixed top-1/2 left-1/2  z-30 flex h-40 w-10/12 -translate-y-1/2 -translate-x-1/2 items-center justify-center rounded-lg bg-stone-200 p-6 text-center shadow-xl hover:cursor-default sm:h-60 sm:p-8 sm:text-lg md:text-xl lg:text-2xl lg2:w-1/2 "
                   onClick={(e: any) => e.stopPropagation()}
                   onKeyDown={(e: any) => e.stopPropagation()}
                   role="button"
                   tabIndex={0}
+                  style={{ backgroundImage: `url(${logosm})` }}
                 >
                   {t('modal')}
                   <button
                     onClick={closeModal}
                     onKeyDown={closeModal}
                     aria-label="close handler"
-                    className="absolute top-10 right-10 text-xl"
+                    className="absolute top-4 right-4 text-xl sm:top-10 sm:right-10"
                   >
-                    X
+                    <AiOutlineClose />
                   </button>
                 </div>
               </div>
