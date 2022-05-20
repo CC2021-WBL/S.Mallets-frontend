dc8bf34117733adb52a9ddf1d3024701dd04ca6e
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-05-20 10:55:23 +0200

    SM-61 removed persist package

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,12 @@ import './i18n';
 
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-toolkit-persist';
 import { Provider } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
-import store, { persistConfig } from './app/store/store';
+import store from './app/store/store';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -17,12 +16,10 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate persistConfig={persistConfig}>
-        <BrowserRouter>
-          <App />
-          <Toaster />
-        </BrowserRouter>
-      </PersistGate>
+      <BrowserRouter>
+        <App />
+        <Toaster />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 );
