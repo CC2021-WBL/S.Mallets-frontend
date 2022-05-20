bec68cb463ea4194829a7991a1a3c1b568b654e8
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-05-20 18:20:50 +0200

    SM-73 added translations

--- src/features/cartPage/ConfirmationPage.tsx
+++ src/features/cartPage/ConfirmationPage.tsx
@@ -1,24 +1,20 @@
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 const ConfirmationPage = () => {
+  const { t } = useTranslation('confirmation');
   return (
     <div className="mt-14  max-w-7xl self-start  px-6 text-xl md:px-9 lg:px-12">
       <div className="max-w-[40rem] ">
-        <p className="mb-5">Dziękujemy! Twoje zamówienie zostało przyjęte.</p>
+        <p className="mb-5">{t('thanks')}</p>
         <p className="mb-5">
-          Numer zamówienia to <span className="font-bold">11003</span>.
+          {t('number')} <span className="font-bold">11003</span>.
         </p>
-        <p className="mb-10">
-          Wkrótce na swojego maila dostaniesz informacje o szacowanym czasie
-          realizacji i sposobie wysyłki.
-        </p>
-        <p>
-          Jeśli masz jakieś pytania skontaktuj się z nami za pomocą formularza
-          kontaktowego.
-        </p>
+        <p className="mb-10">{t('future')}</p>
+        <p>{t('questions')}</p>
 
         <div className="mb-10  mt-12 flex h-[3.75rem] max-w-[27.5rem] items-center justify-center rounded bg-black text-white hover:cursor-pointer hover:opacity-75">
-          <Link to="/contact">FORMULARZ KONTAKTOWY</Link>
+          <Link to="/contact">{t('contact')}</Link>
         </div>
       </div>
     </div>
