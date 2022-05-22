29f966ceaf1f54582652fc2908434a9a71a10d52
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-05-22 18:05:27 +0200

    SM-75 added translations

--- src/common/service/ServicePage.tsx
+++ src/common/service/ServicePage.tsx
@@ -1,34 +1,34 @@
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 import After from '../../assets/sticksAfter.png';
 import Before from '../../assets/sticksBefore.png';
 
 const ServicePage = () => {
+  const { t } = useTranslation('others');
   return (
     <div className=" mx-6 my-12 w-full max-w-7xl px-3 md:px-6 lg:px-8">
       <div className="hero-service mx-4" />
       <div className="mt-12 flex flex-col gap-6 px-4 sm:flex-row ">
         <div className="max-w-full">
-          <p className="mb-10 text-xl font-bold">Serwis</p>
-          <p>
-            Dla nas nie ma rzeczy niemożliwych. Naprawimy wszystkie pałeczki.
-          </p>
-          <p className="mb-10">Masz wątpliwości? Skontaktuj się z nami</p>
+          <p className="mb-10 text-xl font-bold">{t('service')}</p>
+          <p>{t('motto')}</p>
+          <p className="mb-10">{t('doubts')}</p>
           <Link
             to="/contact"
             className="flex h-16 w-full items-center justify-center rounded bg-black text-white hover:opacity-75 md:w-96"
           >
-            Kontakt
+            {t('contact')}
           </Link>
         </div>
         <div className="flex max-w-full flex-col items-end">
-          <p className="self-start">Before</p>
+          <p className="self-start">{t('before')}</p>
           <img
             src={Before}
             alt="sticks before service with frayed heads"
             className="mb-8"
           />
-          <p className="self-start">After</p>
+          <p className="self-start">{t('after')}</p>
           <img src={After} alt="sticks after service without frayed heads" />
         </div>
       </div>
