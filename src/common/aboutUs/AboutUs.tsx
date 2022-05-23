ff516909e9ab35163e3b3200d36605df4b669799
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-05-24 00:43:52 +0200

    SM-74 added translations, changed px to rem

--- src/common/aboutUs/AboutUs.tsx
+++ src/common/aboutUs/AboutUs.tsx
@@ -1,9 +1,12 @@
+import { useTranslation } from 'react-i18next';
+
 const AboutMe = () => {
+  const { t } = useTranslation('about');
   return (
     <div className="mx-6 my-12 w-full max-w-7xl px-3 md:px-6 lg:px-8">
       <div className="hero-about mx-4 mb-14" />
-      <div className=" mx-4 mb-14 h-fit w-full md:w-1/2 lg2:mb-0 lg2:h-[400px] ">
-        <p className="mb-10 text-xl">Kim jestem?</p>
+      <div className=" mx-4 mb-14 h-fit w-full md:w-1/2 lg2:mb-0 lg2:h-[25rem] ">
+        <p className="mb-10 text-xl font-bold">{t('who')}</p>
         <p className="w-[80%]">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum,
           reiciendis! Hic reprehenderit impedit numquam ipsa eos, obcaecati
@@ -11,11 +14,11 @@ const AboutMe = () => {
         </p>
       </div>
 
-      <div className="relative mx-4 mb-14 flex h-[500px] flex-col items-center bg-[#ededed] lg:h-[655px] lg:flex-row">
-        <div className="about-right absolute right-0 -top-[400px] hidden h-[565px] w-[495px] lg2:block" />
-        <div className="about-left mb-14  h-1/2 w-full lg:mb-0 lg:h-[550px] lg:w-1/2 " />
+      <div className="relative mx-4 mb-14 flex h-[31.3rem] flex-col items-center bg-[#ededed] lg:h-[41rem] lg:flex-row">
+        <div className="about-right absolute right-0 -top-[25rem] hidden h-[35.3rem] w-[30.9rem] lg2:block" />
+        <div className="about-left mb-14  h-1/2 w-full lg:mb-0 lg:h-[34.4rem] lg:w-1/2 " />
         <div className="wrap flex w-full flex-col lg:w-1/2 lg:pl-24">
-          <p className="mb-10 text-xl">Co robię?</p>
+          <p className="mb-10 text-xl font-bold">{t('what')}</p>
           <p className="pr-4 pb-4 md:pb-0">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum,
             reiciendis! Hic reprehenderit impedit numquam ipsa eos, obcaecati
