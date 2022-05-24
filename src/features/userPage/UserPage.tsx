666013484b457e47ffb6ec856d79989d926d89ae
Tomasz <skretkowskitomasz@gmail.com>
2022-05-24 21:53:04 +0200

    SM-82 modal few steps further

--- src/features/userPage/UserPage.tsx
+++ src/features/userPage/UserPage.tsx
@@ -1,12 +1,14 @@
 import toast from 'react-hot-toast';
+import { AiOutlineClose } from 'react-icons/ai';
 import { NavLink } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 
 import DeliveryData from '../summaryPage/DeliveryData';
 import LogoCarpet from '../../tools/LogoCarpet';
+import logosm from '../../assets/logosmall.png';
 import { RootState } from '../../app/store';
 import { sectionStyles } from '../summaryPage/SummaryPage';
 import { userWithOrdersActions } from '../../app/userWithOrdersSlice';
@@ -36,8 +38,14 @@ const UserPage = () => {
   const dispatch = useDispatch();
   const { t } = useTranslation('summary');
 
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
   const detailsModalHandler = () => {
     console.log('modal is opened');
+    setShowModal(true);
     //modal takes data from orderSlice ?? or state
   };
 
@@ -116,6 +124,34 @@ const UserPage = () => {
           </NavLink>
         </section>
       </main>
+      {showModal ? (
+        <div
+          onClick={closeModal}
+          onKeyDown={closeModal}
+          role="button"
+          tabIndex={0}
+        >
+          <div className="fixed top-0 left-0  z-20 h-full w-full bg-black opacity-20" />
+          <div
+            className="bg- fixed top-1/2 left-1/2  z-30 flex h-40 w-10/12 -translate-y-1/2 -translate-x-1/2 items-center justify-center rounded-lg bg-stone-200 p-6 text-center shadow-xl hover:cursor-default sm:h-60 sm:p-8 sm:text-lg md:text-xl lg:text-2xl lg2:w-1/2 "
+            onClick={(e: any) => e.stopPropagation()}
+            onKeyDown={(e: any) => e.stopPropagation()}
+            role="button"
+            tabIndex={0}
+            style={{ backgroundImage: `url(${logosm})` }}
+          >
+            {t('modal')}
+            <button
+              onClick={closeModal}
+              onKeyDown={closeModal}
+              aria-label="close handler"
+              className="absolute top-4 right-4 text-xl sm:top-10 sm:right-10"
+            >
+              <AiOutlineClose />
+            </button>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
