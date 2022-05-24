fbe2cb27352190cfcbecba9f8e41465b3ec1269c
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-05-24 18:44:25 +0200

    SM-88 created and styled no access component

---
+++ src/common/NoAccess.tsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+
+function NoAccess() {
+  const { t } = useTranslation('others');
+  return (
+    <div className="mx-6 my-12 flex max-w-7xl flex-col items-center">
+      <h1 className="mb-6 text-center text-4xl">{t('noAccess')}</h1>
+      <h2 className="text-center text-2xl font-bold">{t('noAccessBack')}</h2>
+      <div className="mt-14 flex max-w-[31.25rem] items-center gap-9 rounded-2xl border border-[#E6E6E6] py-7 px-9 font-bold text-[#0050EC]">
+        <Link to="/login" className="w-fit">
+          {t('login')}
+        </Link>
+        <Link to="/" className="w-fit">
+          {t('back')}
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default NoAccess;
